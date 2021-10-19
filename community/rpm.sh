6bf45762e17d12ffff325ac1cd85535fc4d10577
gindibay <gurkanindibay@gmail.com>
2021-10-19 11:18:48 +0300

    Fixes epel problem for ol/7

--- community/rpm.sh
+++ community/rpm.sh
@@ -64,8 +64,6 @@ epel_check()
   else
     echo -n "Installing epel-release repo... "
 
-    echo "OS:${os} dist:${dist}"
-
     if [ "${os}" = "centos" ]; then
       yum install -d0 -e0 -y epel-release &> /dev/null
     elif [ "${os}" = "rhel" ] || [ "${os}" = "redhatenterpriseserver" ] || [ "${os}" = "ol" ]; then
