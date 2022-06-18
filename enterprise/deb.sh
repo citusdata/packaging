0cdee2428fb0da8e8aea546aa9eddb3d27104be1
gindibay <gurkanindibay@gmail.com>
2022-06-18 16:06:39 +0300

    Fixes repo scripts for enterprise and nightlies

--- enterprise/deb.sh
+++ enterprise/deb.sh
@@ -284,7 +284,7 @@ main ()
   echo "Found host ID: ${CITUS_REPO_HOST_ID}"
   gpg_key_install_url="https://repos.citusdata.com/enterprise/gpg_key_url.list?os=${os}&dist=${dist}&name=${CITUS_REPO_HOST_ID}"
 
-  apt_config_url="https://${CITUS_REPO_TOKEN}:@packagecloud.io/install/repositories/citusdata/enterprise/config_file.list?os=${os}&dist=${dist}&name=${unique_id}&source=script"
+  apt_config_url="https://${CITUS_REPO_TOKEN}:@packagecloud.io/install/repositories/citusdata/enterprise/config_file.list?os=${os}&dist=${dist}&name=${CITUS_REPO_HOST_ID}&source=script"
 
   gpg_key_url=`curl -GL -u "${CITUS_REPO_TOKEN}:" --data-urlencode "name=${CITUS_REPO_HOST_ID}" "${gpg_key_install_url}"`
   if [ "${gpg_key_url}" = "" ]; then
