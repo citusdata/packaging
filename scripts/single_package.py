e4657dcf742ebce9fa917c8ec22898cdc2cbc1e9
gindibay <gurkanindibay@gmail.com>
2023-02-06 16:23:08 +0300

    Adds single package build support

--- scripts/single_package.py
+++ scripts/single_package.py
@@ -7,22 +7,22 @@ with open(postgres_matrix_filename) as file:
     data = yaml.full_load(file)
 
 # get the postgres_versions list
-print(f"Version matrix: {data['version_matrix']}")
-postgres_versions = data['version_matrix'][0]['postgres_versions']
+
+version_matrix = data['version_matrix']
+print(f"Version matrix: {version_matrix[0][list(version_matrix[0].keys())[0]]['postgres_versions']}")
+postgres_versions = version_matrix[0][list(version_matrix[0].keys())[0]]['postgres_versions']
 
 # loop through each version and write to a separate file
 for version in postgres_versions:
-
-    data['version_matrix'][0]['postgres_versions'] = [version]
-
+    version_matrix[0][list(version_matrix[0].keys())[0]]['postgres_versions'] = [version]
+    print(f"Running for version:  {version}")
     with open(postgres_matrix_filename, 'w') as file:
         yaml.dump(data, file)
-    result = subprocess.run(
-        ["python", "-m ", "tools.packaging_automation.citus_package", "--gh_token", "'${GH_TOKEN}'", "--platform",
-         "'${PLATFORM}'",
-         "--build_type", "nightly",
-         "--secret_key", "'${PACKAGING_SECRET_KEY}'",
-         "--passphrase", "'${PACKAGING_PASSPHRASE}'",
-         "--output_dir", "$(pwd)/packages/",
-         "--input_files_dir", "$(pwd)/packaging"],
-        text=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        result = subprocess.run(
+            ["python", "-m ", "tools.packaging_automation.citus_package"],
+            capture_output=True)
+        print(result.stderr.decode("utf-8"))
+
+version_matrix[0][list(version_matrix[0].keys())[0]]['postgres_versions'] = postgres_versions
+with open(postgres_matrix_filename, 'w') as file:
+    yaml.dump(data, file)
