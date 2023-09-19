1398f4704d6ac04dc441406864c097e4aae767b7
Hanefi Onaldi <Hanefi.Onaldi@microsoft.com>
2023-09-19 22:43:55 +0300

    Bump PG versions to 16

--- community/rpm.sh
+++ community/rpm.sh
@@ -32,9 +32,9 @@ curl_check ()
 
 pgdg_check ()
 {
-  echo "Checking for postgresql15-server..."
-  if yum list -q postgresql15-server &> /dev/null; then
-    echo "Detected postgresql15-server..."
+  echo "Checking for postgresql16-server..."
+  if yum list -q postgresql16-server &> /dev/null; then
+    echo "Detected postgresql16-server..."
   else
     echo -n "Installing pgdg repo... "
 
@@ -44,8 +44,8 @@ pgdg_check ()
 
     yum install -d0 -e0 -y "${repo_url}"
 
-    if ! yum info -q -y postgresql15-server &> /dev/null; then
-      echo "PGDG repositories don't have postgresql15-server package for your operating system"
+    if ! yum info -q -y postgresql16-server &> /dev/null; then
+      echo "PGDG repositories don't have postgresql16-server package for your operating system"
       echo "Cannot install Citus, exiting."
       exit 1
     fi
--- enterprise/deb.sh
+++ enterprise/deb.sh
@@ -56,9 +56,9 @@ curl_check ()
 
 pgdg_check ()
 {
-  echo "Checking for postgresql-15..."
-  if apt-cache show postgresql-15 &> /dev/null; then
-    echo "Detected postgresql-15..."
+  echo "Checking for postgresql-16..."
+  if apt-cache show postgresql-16 &> /dev/null; then
+    echo "Detected postgresql-16..."
   else
     pgdg_list='/etc/apt/sources.list.d/pgdg.list'
     pgdg_source_path="deb http://apt.postgresql.org/pub/repos/apt/ ${codename}-pgdg main"
@@ -87,8 +87,8 @@ pgdg_check ()
     apt-get update &> /dev/null
     echo "done."
 
-    if ! apt-cache show postgresql-15 &> /dev/null; then
-      echo "PGDG repositories don't have postgresql-15 package for your operating system"
+    if ! apt-cache show postgresql-16 &> /dev/null; then
+      echo "PGDG repositories don't have postgresql-16 package for your operating system"
       echo "Cannot install Citus, exiting."
       exit 1
     fi
